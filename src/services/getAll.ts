d2e4eaff49fe4e44055e5962d98b9423cb7d8540
Pedro Hurtado <pedrohurt@gmail.com>
2024-11-26 16:53:40 +0100

    add axios to query

--- src/services/getAll.ts
+++ src/services/getAll.ts
@@ -1,8 +1,8 @@
-export async function GetAll<T>(url:string):Promise<[T]>{
-    const response= await fetch(url)
-    if(response.status ===200){
-        const data = await response.json() as [T]
-        return data;
+import axios from "axios";
+export async function GetAll<T>(url: string): Promise<[T]> {
+    const response = await axios.get<[T]>(url)
+    if (response.status === 200) {
+        return response.data;
     }
     throw `${response.statusText}->${response.status}`
 }
