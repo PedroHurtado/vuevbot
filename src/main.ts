fb1c52e46af6bd93ec08d8497e91a33a214e2215
Pedro Hurtado <pedrohurt@gmail.com>
2024-12-02 17:57:37 +0100

    multiples stores

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 
 import { createApp } from 'vue'
 import  {router} from './router/routes'
+import { createPinia } from 'pinia';
 import App from './App.vue'
 
+//https://stackoverflow.com/questions/74072848/multiple-instance-of-pinia-store-possible-in-same-page-or-same-component
 const app = createApp(App);
+const pinia = createPinia()
+app.use(pinia)
 app.use(router);
 app.mount('#app')
